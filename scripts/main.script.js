cfbcc766f2d0dc5f61b9bc777e9630cce10ee2bc
Edyta <edyta.zurawska8@gmail.com>
2018-01-20 21:48:07 +0100

    add section projects

--- scripts/main.script.js
+++ scripts/main.script.js
@@ -9,4 +9,27 @@ $(document).ready(function() {
         $('.nav-menu').toggleClass('clicked');
         $('.nav-mobile-btn-bar').toggleClass('clicked');
     });
+    
+    // slider
+    var slideIndex = 1;
+        showSlides(slideIndex);
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+    function showSlides(n) {
+        var i;
+        var slides = $('.mySlides').css('display', 'none');
+        if (n > slides.length) {slideIndex = 1;}
+        if (n < 1) {slideIndex = slides.length;}
+        slides.eq(slideIndex-1).css('display', 'block');
+    }
+    $('.prev').click(function() {
+        plusSlides(-1);
+    });
+    $('.next').click(function() {
+        plusSlides(1);
+    });
 });
