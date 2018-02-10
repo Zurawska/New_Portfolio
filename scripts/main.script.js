e78532d2630a55e4c59aab76290ad558de80af1d
Edyta <edyta.zurawska8@gmail.com>
2018-02-10 20:53:24 +0100

    adjustment section projects to desktops

--- scripts/main.script.js
+++ scripts/main.script.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
         var slides = $('.mySlides').css('display', 'none');
         if (n > slides.length) {slideIndex = 1;}
         if (n < 1) {slideIndex = slides.length;}
-        slides.eq(slideIndex-1).css('display', 'block');
+        slides.eq(slideIndex-1).css('display', 'flex');
     }
     $('.prev').click(function() {
         plusSlides(-1);
